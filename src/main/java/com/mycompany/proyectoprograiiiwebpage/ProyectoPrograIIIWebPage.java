12839f7886734f51c29f54ebfc4a17dce7491bd2
Juanes El Patron <juanesgober23@hotmail.com>
2023-05-27 21:14:23 -0500

    Agregación proyecto

---
+++ src/main/java/com/mycompany/proyectoprograiiiwebpage/ProyectoPrograIIIWebPage.java
@@ -0,0 +1,8 @@
+package com.mycompany.proyectoprograiiiwebpage;
+
+public class ProyectoPrograIIIWebPage {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
